01730777ff92ed3d3034190c7e3c1a1605c9c7cd
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-11-21 16:30:45 -0800

    Added FPS counter

--- MissileCommand/MainWindow.xaml.cs
+++ MissileCommand/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace MissileCommand
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double t;
+        private double t, fps;
         private Random rand = new Random();
 
         public MainWindow()
@@ -40,11 +40,13 @@ namespace MissileCommand
             GameObject.Initialize(Screen);
 
             new DelayedAction(1.0, () => new Trail(new(random(0, 800), random(0, 400)), new(random(0, 800), random(0, 400)), random(50, 500)), true);
+            new DelayedAction(0.25, () => FpsCounter.Text = $"{fps:0} FPS", true);
         }
 
         private void Update(double dt)
         {
             t += dt;
+            fps = fps * 0.9 + (1.0 / dt) * 0.1;
             GameObject.UpdateAll(dt);
         }
 
