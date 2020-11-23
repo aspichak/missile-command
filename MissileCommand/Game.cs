e455f949bfc7cd9137ce3188237297a4506cddf4
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-11-23 02:56:28 -0800

    Static Game class

---
+++ MissileCommand/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MissileCommand
+{
+    static class Game
+    {
+        public static int Score { get; set; }
+        public static int Round { get; set; }
+        public static bool Paused { get; set; }
+
+        public static List<GameObject> Enemies;
+        public static List<GameObject> Silos;
+        public static List<GameObject> Cities;
+
+        public static GameObject NearestSilo(Vector vector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
