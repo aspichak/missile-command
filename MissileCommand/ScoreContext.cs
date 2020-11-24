6ca3ee16964375d331cc0ea4b344f0005cb2e51e
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-11-23 20:17:09 -0800

    basic score DB context created

---
+++ MissileCommand/ScoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MissileCommand
+{
+    class ScoreContext : DbContext
+    {
+        public DbSet<Score> Scores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=scores.db");
+    }
+    public class Score
+    {
+        public int ScoreId { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+    }
+}
