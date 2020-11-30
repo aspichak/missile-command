91e62ce89efed29903c41b9ab0bbf3feab6ed8fc
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-11-29 21:07:38 -0800

    silo skeleton

---
+++ MissileCommand/Silo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MissileCommand
+{
+    class Silo : GameElement, ITargetable
+    {
+        private bool infiniteAmmo;
+        public Vector TargetPosition { get; }
+        private Vector Position { get; set; }
+        public void Explode()
+        {
+            // does that boom boom thing
+        }
+        public void Rebuild()
+        {
+            // gets it back up
+        }
+
+        public Silo(Vector position, bool infAmmo = false)
+        {
+            Position = position;
+            infiniteAmmo = infAmmo;
+        }
+    }
+}
