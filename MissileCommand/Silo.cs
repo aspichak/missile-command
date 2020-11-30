5476cc9393fac918d48f5be72ac33d8ab5df9d57
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-11-29 21:19:06 -0800

    basic icommand skeleton logic added

--- MissileCommand/Silo.cs
+++ MissileCommand/Silo.cs
@@ -4,22 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace MissileCommand
 {
-    class Silo : GameElement, ITargetable
+    class Silo : GameElement, ITargetable, ICommand
     {
         private bool infiniteAmmo;
-        public Vector TargetPosition { get; }
+        private bool destroyed = false;
         private Vector Position { get; set; }
+
+        #region ITargetable contract
+        public Vector TargetPosition { get; }
         public void Explode()
         {
             // does that boom boom thing
+            destroyed = true;
         }
         public void Rebuild()
         {
             // gets it back up
+            destroyed = false;
         }
+        #endregion
+        #region ICommand contract
+        public void Execute(object parameter)
+        {
+            /*if (screen.Paused) return;
+            var pos = Mouse.GetPosition(screen.GameScreenGrid);
+            if (pos.X > 0 && pos.Y > 0)
+                new Missile(new(X, Y), new(pos.X, pos.Y), 400);*/
+        }
+
+        public bool CanExecute(object parameter) { return !destroyed; }
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        #endregion
 
         public Silo(Vector position, bool infAmmo = false)
         {
