8e79cf1086b2f7aba14537ea537c1780d99b91e1
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-12-02 21:38:58 -0800

    super evil self targeting missiles complete!

--- MissileCommand/EnemyMissile.cs
+++ MissileCommand/EnemyMissile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace MissileCommand
@@ -11,10 +13,24 @@ namespace MissileCommand
         public Vector Position { get; private set; }
         public ITargetable Target { get; set; }
         public event Action Exploded;
+        private Vector From { get; set; }
+        private double Speed { get; set; }
 
-        public EnemyMissile(Vector from, Vector to, double speed)
+        public EnemyMissile(Vector from, double speed)
         {
-            trail = new Trail(from, to, speed, Colors.Orange, Colors.OrangeRed);
+            Loaded += EnemyMissile_Loaded;
+            Speed = speed;
+            From = from;
+        }
+
+        private void EnemyMissile_Loaded(object sender, RoutedEventArgs e)
+        {
+            var targets = from item in ((Canvas)Parent).Children.OfType<ITargetable>()
+                          where item.IsDestroyed == false
+                          select item;
+            var target = targets.Random();
+            this.Target = target;
+            trail = new Trail(From, Target.TargetPosition, Speed, Colors.Orange, Colors.OrangeRed);
             trail.Moving += pos => Position = pos;
             trail.Completed += TargetReached;
 
