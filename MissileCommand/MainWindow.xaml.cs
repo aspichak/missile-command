0e906a2127f33772c7f94c324ab6bac65ccf8f65
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-12-07 19:51:08 -0800

    Added a README

--- MissileCommand/MainWindow.xaml.cs
+++ MissileCommand/MainWindow.xaml.cs
@@ -1,4 +1,27 @@
-﻿using MissileCommand.Screens;
+﻿/*
+ * Missile command game clone by Jeff Nix and Alex Spichak. This project requires [.NET SDK 5.0](https://dotnet.microsoft.com/download/dotnet/5.0) and Visual Studio 2019 16.8.0 or later to compile. 
+ * 
+ * Controls:
+ *     - Aim with the mouse and press 1, 2, or 3 to launch a missile from the corresponding silo.
+ *     - Press Escape to pause the game.
+ * 
+ * Extra credit features:
+ *     - Custom InputBindings
+ *     - Custom Command for bindings
+ *     - Super fun dynamic LINQ shenanigans!
+ *     - Database is Entity Framework
+ *     - XAML event triggers
+ *     - Databinding used in some places
+ *     - Controller? What controller! Is MVVM baby
+ *     - Did you see that happen? No? Well not my fault you didn’t subscribe to the event!
+ *     - Buttery smooth animations
+ *     - Screen flash / shake effects
+ *     - Background music and sound effects
+ *     - Proper scaling on window resize
+ *     - Microtransaction-free
+ */
+
+using MissileCommand.Screens;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
