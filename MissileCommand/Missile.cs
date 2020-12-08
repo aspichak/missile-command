70b4b2e1bae117d7a059e29ffbfcdf8d11e6f1dc
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-12-07 16:34:21 -0800

    Added player missile destination X

--- MissileCommand/Missile.cs
+++ MissileCommand/Missile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace MissileCommand
 {
@@ -16,6 +17,24 @@ namespace MissileCommand
             trail.Moving += pos => Position = pos;
             trail.Completed += Explode;
 
+            var cross1 = new Line();
+            cross1.X1 = to.X - 4;
+            cross1.Y1 = to.Y - 4;
+            cross1.X2 = to.X + 4;
+            cross1.Y2 = to.Y + 4;
+            cross1.Stroke = new SolidColorBrush(Colors.BlueViolet);
+            cross1.StrokeThickness = 2;
+
+            var cross2 = new Line();
+            cross2.X1 = to.X - 4;
+            cross2.Y1 = to.Y + 4;
+            cross2.X2 = to.X + 4;
+            cross2.Y2 = to.Y - 4;
+            cross2.Stroke = new SolidColorBrush(Colors.BlueViolet);
+            cross2.StrokeThickness = 2;
+
+            Add(cross1);
+            Add(cross2);
             AddToParent(trail);
         }
 
