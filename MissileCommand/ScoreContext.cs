6f3c0797da69c06a5aa78072243b4798ceaa998d
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-12-07 18:14:04 -0800

    the great using prune

--- MissileCommand/ScoreContext.cs
+++ MissileCommand/ScoreContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.IO;
 using System.ComponentModel.DataAnnotations;
 
--- MissileCommand/Screens/IngameScreen.xaml.cs
+++ MissileCommand/Screens/IngameScreen.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Media;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
